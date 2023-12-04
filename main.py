ee60198b1eddff5aed2e49a66070a615c2404213
dan1183 <klandrtp@ya.ru>
2023-12-04 13:13:59 +0300

    Добавлено создание открытого и закрытого ключей e и d, а так же функция нахождения всех делителей числа

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def is_it_prime(n):
     d = 2
     while d < n:
@@ -15,3 +18,33 @@ def get_prime_numbers(fi=200):
         if is_it_prime(i):
             prime_numbers.append(i)
     return prime_numbers
+
+
+def find_all_dividers(n):
+    d = 2
+    dividers = set()
+    while d <= n:
+        if n % d != 0:
+            d += 1
+        else:
+            dividers.add(d)
+            d += 1
+    return dividers
+
+
+def find_e(fi):
+    flag = True
+    while flag == True:
+        e = random.choice(range(1000))
+        if e % 2 == 0:
+            e -= 1
+        flag = (bool(find_all_dividers(e) & find_all_dividers(fi)))
+    return e
+
+
+def find_d(e, fi):
+    # d - число обратное е по модулю fi => (d * e) % fi = 1
+    d = 1
+    while (d * e) % fi != 1:
+        d += 1
+    return d
